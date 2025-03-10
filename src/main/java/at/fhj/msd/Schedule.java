61995e18fc277efd7953a8ccecc62f02f06f7bb6
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-11 00:40:55 +0100

    Added Schedule.java in last commit to git repository and in this commit I  implemented needed attributes and methods

---
+++ src/main/java/at/fhj/msd/Schedule.java
@@ -0,0 +1,24 @@
+package at.fhj.msd;
+
+public class Schedule {
+
+    private String cohort;
+    private String group;
+    private String lecture;
+    private String starts;
+    private String ends;
+    private String lecturer;
+    private String place;
+
+    
+
+
+    public String asCsv(String delimeter) {
+        return "";
+    }
+
+    public String asSql() {
+        return "";
+    }
+
+}
