eded354e8da33dcdf3c1353337c2f211579444d8
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-11 01:23:58 +0100

    Changed order from attributs in Schedule.java! Implemented read Method

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,10 +1,38 @@
 package at.fhj.msd;
 
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        String filename = "src\\main\\resources\\data.txt";
+        ArrayList<String> listOfSchedules = read(filename);
+        
+
+
     }
+
+    public static ArrayList<String> read(String filename) {
+
+        File file = new File(filename);
+
+        ArrayList<String> lines = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while((line = br.readLine()) != null) {
+               lines.add(line);
+            }
+        }catch (IOException e) {
+            System.out.printf("Error during reading the file '%s'", file);
+            return null;
+        }
+        return lines;
+    }
+
+
 }
--- src/main/java/at/fhj/msd/Schedule.java
+++ src/main/java/at/fhj/msd/Schedule.java
@@ -3,15 +3,24 @@ package at.fhj.msd;
 public class Schedule {
 
     private String cohort;
-    private String group;
     private String lecture;
+    private String group;
     private String starts;
     private String ends;
-    private String lecturer;
     private String place;
+    private String lecturer;
 
-    
+    public Schedule(String cohort, String group, String lecture, String starts, String ends, String lecturer, String place) {
+            
+        this.cohort = cohort;
+        this.lecture = lecture;
+        this.group = group;
+        this.starts = starts;
+        this.ends = ends;
+        this.place = place;
+        this.lecturer = lecturer;
 
+    }
 
     public String asCsv(String delimeter) {
         return "";
